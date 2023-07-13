05bc86b5fb3f7c0910fb03f9d114ed7f1cf8f3ed
xacah-x <xacahmytl@gmail.com>
2023-07-13 15:28:02 +0300

    create and update validator classes updated

--- WebApi/BookOperations/UpdateBook/UpdateBookCommandValidator.cs
+++ WebApi/BookOperations/UpdateBook/UpdateBookCommandValidator.cs
@@ -5,7 +5,7 @@ namespace WebApi.BooksOperations.UpdateBook
     {
         public UpdateBookCommandValidator()
         {
-            RuleFor(command => command.Model.GenreId).GreaterThan(0);
+            RuleFor(command => command.Model.GenreId).GreaterThan(0).IsInEnum();
             RuleFor(command => command.Model.PageCount).GreaterThan(0);
             RuleFor(command => command.Model.Title).NotEmpty().MinimumLength(1);
         }
